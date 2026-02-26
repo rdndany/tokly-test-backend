03ba71d0af8b903aa1ed938ecd9b4602f7e7e38f
rdndany <iliedanielmarian@gmail.com>
2026-02-26 02:29:20 +0200

    sf

--- src/routes/ga4Routes.ts
+++ src/routes/ga4Routes.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { checkAuth } from "../middlewares/auth";
-import { getGA4Analytics, getGA4Status, getGA4Realtime } from "../controllers/GA4Controller";
+import { getGA4Analytics, getGA4Status } from "../controllers/GA4Controller";
 
 const router = Router();
 
 router.get("/project/:projectId", checkAuth, getGA4Analytics);
 router.get("/status", checkAuth, getGA4Status);
-router.get("/realtime", checkAuth, getGA4Realtime);
 
 export default router;
