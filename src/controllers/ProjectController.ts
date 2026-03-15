fa08180f67c48310c6e9dac5f70bb2d8c5dff55f
rdndany <iliedanielmarian@gmail.com>
2026-03-15 21:28:25 +0200

    sf solana send all

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -2583,7 +2583,13 @@ export async function continueSolanaDistribution(
 
     const writeLine = (obj: object) => {
       res.write(JSON.stringify(obj) + "\n");
+      (res as Response & { flush?: () => void }).flush?.();
     };
+
+    let requestAborted = false;
+    req.on("aborted", () => {
+      requestAborted = true;
+    });
 
     try {
       const result = await runRelay({
@@ -2592,6 +2598,7 @@ export async function continueSolanaDistribution(
         recipientAddresses,
         amountPerRecipientBase,
         cluster,
+        isAborted: () => requestAborted,
         onBatchComplete: async (signature) => {
           const distribution = await getDistributionFromS3(projectId);
           if (distribution?.batches) {
